d900a514bec695e6e848caadb0a96e5581c2c59e
Rafael Castillo <rafcasto@gmail.com>
2023-06-08 22:42:14 +1200

    adding cross site origin

--- src/main/java/org/tax/calculator/controllers/TaxCalculatorController.java
+++ src/main/java/org/tax/calculator/controllers/TaxCalculatorController.java
@@ -2,14 +2,11 @@ package org.tax.calculator.controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tax.calculator.components.TaxCalculatorComponent;
 import org.tax.calculator.models.CalculateTaxRequest;
 import org.tax.calculator.models.CalculateTaxResponse;
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class TaxCalculatorController
